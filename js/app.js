30e98611f70fac7bde44787561eec89cf2701c92
Vaibhav1671998 <vk530873@gmail.com>
2017-11-23 14:41:15 +0530

    Added drop-down menu

--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
+var voicelist = responsiveVoice.getVoices();
+for (v in voicelist) {
+   console.log(voicelist[v].name); 
+   $('#voice').append('<option id="' + voicelist[v].name + '" value="' + voicelist[v].name + '">' + voicelist[v].name + '</option>');
+}
+
 function enterpressalert(e, input){
-	var code = (e.keyCode ? e.keyCode : e.which);
-	if(code == 13) { 
-		text = document.getElementById("text").value;
-		responsiveVoice.speak(text);
-	}
+	if(responsiveVoice.voiceSupport()) {
+      var code = (e.keyCode ? e.keyCode : e.which);
+      if(code == 13) { 
+         text = document.getElementById("text").value;
+         selectedVoice = $('#voice :selected').text()
+         responsiveVoice.speak(text,selectedVoice);
+      }
+   }
+   else {
+      alert("Your browser doesn't support this app")
+   }  
 }
 document.getElementById("demo").addEventListener('click',function () {
-	text = document.getElementById("text").value;
-	responsiveVoice.speak(text);
+	if(responsiveVoice.voiceSupport()) {
+      text = document.getElementById("text").value;
+      selectedVoice = $('#voice :selected').text()
+      responsiveVoice.speak(text,selectedVoice);
+   }
+   else {
+      alert("Your browser doesn't support this app")
+   }  
 });
